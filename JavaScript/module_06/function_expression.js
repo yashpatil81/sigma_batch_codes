e6a711405768500884bc326380fb09bd1541ca91
Yashpatil81 <lucky91127@gmail.com>
2025-03-18 00:13:59 +0530

    module 6.5

---
+++ JavaScript/module_06/function_expression.js
@@ -0,0 +1,19 @@
+let sum = function(a,b){
+    return a+b
+}
+
+console.log(sum(1,2));
+
+let hello = function(){
+    console.log('hello');
+    
+}
+
+hello = function(){
+    console.log('namaste');
+    
+}
+hello()
+
+
+
