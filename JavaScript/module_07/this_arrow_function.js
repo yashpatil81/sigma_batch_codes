e9d6af4f2fd30322cdcc24f1be7d4976a47e0819
Yashpatil81 <lucky91127@gmail.com>
2025-03-27 11:34:56 +0530

    7.9

--- JavaScript/module_07/this_arrow_function.js
+++ JavaScript/module_07/this_arrow_function.js
@@ -1,18 +1,18 @@
-const  student = {
-    name: 'aman',
-    marks : 95,
-    prop: this, // global scope  [here this represents the windowss object ]
-    getName : function() {
-        console.log(this);
-        return this.name        
-    },
-    getMarks : () => {
-        console.log(this);
-        return this.name           
-    }
-};
+// const  student = {
+//     name: 'aman',
+//     marks : 95,
+//     prop: this, // global scope  [here this represents the windowss object ]
+//     getName : function() {
+//         console.log(this);
+//         return this.name        
+//     },
+//     getMarks : () => {
+//         console.log(this);
+//         return this.name           
+//     }
+// };
 
-student.getName()
+// student.getName()
 
 // normal function ke liye this uska calling object hota hai lekin 
 // arrow function ke liye this uska parent's scope hota hai
