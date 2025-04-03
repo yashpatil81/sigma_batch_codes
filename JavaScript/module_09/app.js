caeab66dcb63d2efb48ed0a4cbaeaad62cac96bb
Yashpatil81 <lucky91127@gmail.com>
2025-04-03 10:00:13 +0530

    9 started

---
+++ JavaScript/module_09/app.js
@@ -0,0 +1,7 @@
+let smallImages = document.getElementsByClassName("oldImg")
+
+for(let i =0; i<smallImages.length;i++){
+    console.dir(smallImages[i]);
+    console.dir(smallImages[i].src)
+    
+}
