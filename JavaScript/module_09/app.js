75f908e520bf84ec55c86b4a01a6281ca675de00
Yashpatil81 <lucky91127@gmail.com>
2025-04-03 18:23:44 +0530

    9.5 completed

--- JavaScript/module_09/app.js
+++ JavaScript/module_09/app.js
@@ -1,7 +1,40 @@
-let smallImages = document.getElementsByClassName("oldImg")
+// let smallImages = document.getElementsByClassName("oldImg")
 
-for(let i =0; i<smallImages.length;i++){
-    console.dir(smallImages[i]);
-    console.dir(smallImages[i].src)
+// // document.getElementsByClassName will always give HTML Collection
+// // it is similar to array but we can only iterate on it 
+// // push,pop and other methods are not allowed
+
+// for(let i =0; i<smallImages.length;i++){
+//     // console.dir(smallImages[i]);
+//     // console.dir(smallImages[i].src)
+
+//     smallImages[i].src = "assets/spiderman_img.png" 
+//     console.log(`value of image no ${i} is changed`)
     
-}
+// }
+
+// query selector does not give html collection it only use to select single elements
+
+console.dir(document.querySelectorAll('p'))
+
+// console.dir(document.querySelector('#description'))
+
+// console.dir(document.querySelector('.oldImg'))
+
+// div tag ke ander anchor tag chahiye 
+// sirf div ke andar ka pahla a tag select hoga with class :- boxlink
+// console.dir(document.querySelector( 'div a'));
+
+// console.dir(document.querySelectorAll( 'div a')); // here we get aa node list of 4 items 
+
+
+// textContent dikhata hai ki humne index . html me ky likha hua hai
+// har line ke baad /n dikhata hai 
+// it shows all the hidden tags inside that element
+
+// innerText dikhata hai ki hamari screen pe kya print hua hai
+
+// innerHtml dikhata hai ki HTML ka markup kya tha 
+// it shows tags
+
+
---
+++ JavaScript/module_09/app1.js
@@ -0,0 +1,18 @@
+para = document.querySelector('p')
+para.innerText = 'abc'
+
+para.innerText = 'Hi, i am Peter Parker!'
+
+para.innerText = 'Hi, i am <b> Peter Parker </b>!' // ye sab chiz text he cconsider karta hai
+
+para.innerHTML = 'Hi, i am <b> Peter Parker </b>!' 
+
+// jaha hame HTML markup(tags like bold , italic etc.) ko bhi pass karna 
+// hota hai vaha innerHTML ka use hota hai
+
+heading = document.querySelector('h1');
+// heading.innerHTML = '<u> Spider Man </u>'
+
+// if we dont want to recall the heading (spider man) then we can also inject variables
+
+heading.innerHTML = `<u> ${heading.innerText} </u>`
