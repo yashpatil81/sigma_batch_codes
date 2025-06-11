45ed541187be7e33e5fba2d73eb86f33ca751232
Yashpatil81 <lucky91127@gmail.com>
2025-06-11 12:55:00 +0530

    Dom 10.5 completed

---
+++ JavaScript/module_10/index2.js
@@ -0,0 +1,28 @@
+// let btn = document.querySelector('button')
+// let p = document.querySelector('p')
+// let h1 = document.querySelector('h1')
+// let h3 = document.querySelector('h3')
+
+
+// btn.addEventListener('click', function() {
+//     console.dir(this);
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = 'blue';
+// })
+
+// // here this refers to btn .
+
+// function changeColor(){
+//     console.dir(this.innerText)
+//     this.style.backgroundColor= 'blue'
+// }
+
+// btn.addEventListener('click',changeColor)
+// p.addEventListener('click',changeColor)
+// h1.addEventListener('click',changeColor)
+// h3.addEventListener('click',changeColor)
+
+/////****************************************************************************************** */
+
+
+
